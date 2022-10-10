2b4dba1fd044714a373db18d68904c6375467756
Gustav Corpas <gustavcorpas@gmail.com>
2022-10-10 16:57:51 +0200

    Robot robbers suff

--- robot-robbers/ownmodel.py
+++ robot-robbers/ownmodel.py
@@ -1,39 +1,71 @@
+from cmath import log
+import gym
 from game.environment import RobotRobbersEnv
-import random
+import numpy as np
+from tf_agents import trajectories
+from gym import spaces
 
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
+class RobotRobbersEnvWrapper(gym.Env):
+  def __init__(self):
+    self.env = RobotRobbersEnv()
+    self.action_space = spaces.Discrete(9)
+    self.observation_space = spaces.Box(
+            low=-1,
+            high=128,
+            shape=(53,),
+            dtype=np.int32
+        )
 
-env = RobotRobbersEnv()
+  def step(self, action):
+    theaction = self._map_action(action)
+    self.env.render()
+    obs, reward, done, info = self.env.step(theaction)
+    obs = self._map_observation(obs)
+    return (obs, reward, done, info)
 
-env.max_n_scrooges = 0
-env.max_n_cashbags = 5
-env.max_n_dropspots = 3
+  def reset(self):
+    obs =  self.env.reset()
+    obs = self._map_observation(obs)
+    return obs
 
-state = env.reset()
-
-
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(6, 10, 4)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(env.n_robbers * 2)
-])
-moves = [random.randint(-1,1) for _ in range(env.n_robbers * 2)]
-state, reward, is_done, info = env.step(moves)
-prediction = model([state]);
-print(_)
+  def render(self, mode='human', close=False):
+    self.env.render()
 
 
-i = 0
+  def _map_action(self, action_number):   
+    if action_number == 0:
+        a = [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0] # GO LEFT
+    elif action_number == 1:
+        a = [0, -1, 0, 0, 0, 0, 0, 0, 0, 0] # GO DOWN
+    elif action_number == 2:
+        a = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0] # GO UP
+    elif action_number == 3:
+        a = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0] # GO RIGHT
+    elif action_number == 4:
+        a = [1, -1, 0, 0, 0, 0, 0, 0, 0, 0] # GO UPLEFT
+    elif action_number == 5:
+        a = [-1, -1, 0, 0, 0, 0, 0, 0, 0, 0] # GO DOWNLEFT
+    elif action_number == 6:
+        a = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0] # GO UPRIGHT
+    elif action_number == 7:
+        a = [-1, 1, 0, 0, 0, 0, 0, 0, 0, 0] # GO DOWNRIGHT
+    elif action_number == 8:
+        a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # STAY
+    
+    return a
 
-while i < 5:
-  i += 1
-  moves = [random.randint(-1,1) for _ in range(env.n_robbers * 2)]
-  state, reward, is_done, info = env.step(moves)
-  print(moves)
-  print(state)
-  print(reward)
-  print(is_done)
-  print(info)
+  def _map_observation(self, observation):
+      a = np.array([])
+      for i in range(1):
+          a = np.append(a, observation[0][i][0:2]) # PLAYERS
+      for i in range(7):
+          a = np.append(a, observation[1][i][0:2]) # ENEMIES
+      for i in range(5):
+          a = np.append(a, observation[2][i][0:2]) # CASH
+      for i in range(3):
+          a = np.append(a, observation[3][i][0:2]) # DROPSPOTS
+      for i in range(5):
+          a = np.append(a, observation[4][i][0:4]) # OBSTACLES
+      for i in range(1):
+          a = np.append(a, observation[5][i][0:1]) # CARIED CASH
+      return a
